cc3bf7dee94c8e53d114bcc0562cd8303d9c5f18
isold <isold.wang@qq.com>
2024-04-20 11:23:29 +0800

    update code

--- src/backend/base/langflow/services/database/models/user/crud.py
+++ src/backend/base/langflow/services/database/models/user/crud.py
@@ -20,6 +20,7 @@ def get_user_by_id(db: Session, id: UUID) -> Union[User, None]:
 
 
 def update_user(user_db: Optional[User], user: UserUpdate, db: Session = Depends(get_session)) -> User:
+    print("bbbbbbbbbbbbbbbbbbbbbbbbbbbb")
     if not user_db:
         raise HTTPException(status_code=404, detail="User not found")
 
